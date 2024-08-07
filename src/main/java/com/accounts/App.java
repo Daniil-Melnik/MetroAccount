3b6f2530104d2d08a092a832e794906504a46a07
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-07 23:12:46 +0300

    --

--- src/main/java/com/accounts/App.java
+++ src/main/java/com/accounts/App.java
@@ -2,10 +2,13 @@ package com.accounts;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+// import java.awt.Color;
+
 import javax.swing.JButton;
 
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
+// import java.awt.FlowLayout;
+// import java.awt.BorderLayout;
 
 public class App extends JFrame {
 
@@ -14,6 +17,9 @@ public class App extends JFrame {
     private JButton listCarriagesButton;
 
     public App() {
+
+        setLayout(null);
+
         setTitle("Управление поездами");
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,15 +27,19 @@ public class App extends JFrame {
         setSize(400, 300);
 
         buttonPanel = new JPanel();
-        buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.setLayout(null);
+        buttonPanel.setBounds(0, 0, 400, 100);
 
         addCarriageButton = new JButton("Добавить вагон");
+        addCarriageButton.setBounds(0, 0, 100, 30);
+
         listCarriagesButton = new JButton("Список вагонов");
+        listCarriagesButton.setBounds(85, 0, 100, 30);
 
         buttonPanel.add(addCarriageButton);
         buttonPanel.add(listCarriagesButton);
 
-        add(buttonPanel, BorderLayout.NORTH);
+        add(buttonPanel);
         
         setVisible(true);
     }
