4a9b19314f994ee2e1f515b55ba1321571f21305
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-07 22:57:10 +0300

    added simple panel

--- src/main/java/com/accounts/App.java
+++ src/main/java/com/accounts/App.java
@@ -1,13 +1,38 @@
 package com.accounts;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+
+public class App extends JFrame {
+
+    public App() {
+        setTitle("Управление поездами");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setSize(400, 300);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JButton addCarriageButton = new JButton("Добавить вагон");
+        JButton listCarriagesButton = new JButton("Список вагонов");
+
+        buttonPanel.add(addCarriageButton);
+        buttonPanel.add(listCarriagesButton);
+
+        add(buttonPanel, BorderLayout.NORTH);
+        
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        // Вызов конструктора для создания и отображения окна
+        new App();
     }
 }
+
