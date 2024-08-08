4f2a2dd6bf444788c9f910c8ccd931ecd35dac9f
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-08 22:51:24 +0300

    4 -- some changes in HeadMenu.java

--- src/main/java/com/accounts/App.java
+++ src/main/java/com/accounts/App.java
@@ -1,12 +1,8 @@
 package com.accounts;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-// вынести панель кнопок в отдельный компонент JPanel
 
 import com.accounts.panels.HeadMenu;
-
-import javax.swing.JButton;
 
 public class App extends JFrame {
 
@@ -15,25 +11,13 @@ public class App extends JFrame {
     public App() {
 
         setLayout(null);
-
         setTitle("Управление поездами");
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         setSize(400, 300);
 
         buttonPanel = new HeadMenu();
-        // buttonPanel.setLayout(null);
-        buttonPanel.setBounds(0, 0, 400, 100);
 
-        // addCarriageButton = new JButton("Добавить");
-        // addCarriageButton.setBounds(0, 0, 110, 25);
-
-        // listCarriagesButton = new JButton("Список");
-        // listCarriagesButton.setBounds(115, 0, 110, 25);
-
-        // buttonPanel.add(addCarriageButton);
-        // buttonPanel.add(listCarriagesButton);
+        buttonPanel.setBounds(0, 0, 400, 50);
 
         add(buttonPanel);
         
--- src/main/java/com/accounts/panels/HeadMenu.java
+++ src/main/java/com/accounts/panels/HeadMenu.java
@@ -11,13 +11,13 @@ public class HeadMenu extends JPanel {
     public HeadMenu(){
         setLayout(null);
 
-        setSize(400, 100);
+        setSize(400, 50);
 
         addCarriageButton = new JButton("Добавить");
-        addCarriageButton.setBounds(0, 0, 110, 25);
+        addCarriageButton.setBounds(0, 0, 110, 20);
 
         listCarriagesButton = new JButton("Список");
-        listCarriagesButton.setBounds(115, 0, 110, 25);
+        listCarriagesButton.setBounds(110, 0, 110, 20);
 
         add(addCarriageButton);
         add(listCarriagesButton);
