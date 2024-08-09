0fa2f344ab7c5504242013a32d3de3505d7d3f13
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-09 20:32:54 +0300

    8 -- Prepared ListPanel.java

--- src/main/java/com/accounts/panels/ListPanel.java
+++ src/main/java/com/accounts/panels/ListPanel.java
@@ -1,5 +1,21 @@
 package com.accounts.panels;
 
-public class ListPanel {
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ListPanel extends JPanel{
+
+    private JLabel titleLable;
+
+    public ListPanel(){
+        setLayout(null);
+        setSize(400, 280);
+
+        titleLable = new JLabel("Список Вагонов");
+        
+        titleLable.setBounds(150, 0, 100, 20);
+
+        add(titleLable);
+    }
     
 }
