230273215ff4abadfb688aabecd2997221413aec
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-20 16:43:53 +0300

    added date pecker to addPanel

--- src/main/java/com/accounts/panels/AddPanel.java
+++ src/main/java/com/accounts/panels/AddPanel.java
@@ -1,20 +1,32 @@
 package com.accounts.panels;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import org.jdesktop.swingx.JXDatePicker;
 
 public class AddPanel extends JPanel {
 
     private JLabel titleLable;
+    private JXDatePicker datePicker;
 
     public AddPanel() {
         setLayout(null);
         setSize(400, 280);
 
+        datePicker = new JXDatePicker();
+        datePicker.setDate(Calendar.getInstance().getTime());
+        datePicker.setFormats(new SimpleDateFormat("dd.MM.yyyy"));
+
         titleLable = new JLabel("Добавить вагон");
 
         titleLable.setBounds(150, 0, 100, 20);
+        datePicker.setBounds(20, 30, 120, 20);
 
         add(titleLable);
+        add(datePicker);
     }
 }
