8e62a522e20f968d8702e96b6059c7438ddaef8c
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-20 17:31:09 +0300

    added TimePicker.java

--- src/main/java/com/accounts/components/TimePicker.java
+++ src/main/java/com/accounts/components/TimePicker.java
@@ -1,6 +1,7 @@
 package com.accounts.components;
 
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TimePicker extends JPanel {
@@ -9,6 +10,9 @@ public class TimePicker extends JPanel {
 
     private JComboBox<String> hourCombobox;
     private JComboBox<String> minuteCombobox;
+
+    private JLabel hourLabel;
+    private JLabel minutLabel;
 
     private void makeHourVars() {
         hourVars = new String[24];
@@ -29,14 +33,22 @@ public class TimePicker extends JPanel {
         makeMinutesVars();
 
         setLayout(null);
-        setSize(200, 30);
-        hourCombobox = new JComboBox<String>(hourVars);
-        minuteCombobox = new JComboBox<String>(minuteVars);
+        setSize(250, 30);
 
-        hourCombobox.setBounds(0, 0, 100, 30);
-        minuteCombobox.setBounds(105, 0, 100, 30);
+        hourCombobox = new JComboBox<String>(hourVars);
+        hourCombobox.setSelectedIndex(8);
+        minuteCombobox = new JComboBox<String>(minuteVars);
+        hourLabel = new JLabel("ч.");
+        minutLabel = new JLabel("мин.");
+
+        hourCombobox.setBounds(0, 0, 50, 25);
+        minuteCombobox.setBounds(70, 0, 50, 25);
+        hourLabel.setBounds(53, 0, 20, 25);
+        minutLabel.setBounds(123, 0, 80, 25);
 
         add(hourCombobox);
         add(minuteCombobox);
+        add(hourLabel);
+        add(minutLabel);
     }
 }
--- src/main/java/com/accounts/panels/AddPanel.java
+++ src/main/java/com/accounts/panels/AddPanel.java
@@ -8,13 +8,15 @@ import javax.swing.JPanel;
 
 import org.jdesktop.swingx.JXDatePicker;
 
+import com.accounts.components.TimePicker;
+
 public class AddPanel extends JPanel {
 
     private JLabel titleLable;
     private JXDatePicker datePicker;
-    // private JXDatePicker timePicker;
+    private TimePicker timePicker;
     private JLabel dateLabel;
-    // private JLabel timeLabel;
+    private JLabel timeLabel;
 
     public AddPanel() {
         setLayout(null);
@@ -24,24 +26,22 @@ public class AddPanel extends JPanel {
         datePicker.setDate(Calendar.getInstance().getTime());
         datePicker.setFormats(new SimpleDateFormat("dd.MM.yyyy"));
 
-        // timePicker = new JXDatePicker();
-        // timePicker.setDate(Calendar.getInstance().getTime());
-        // timePicker.setFormats(new SimpleDateFormat("h:mm a"));
+        timePicker = new TimePicker();
 
         titleLable = new JLabel("Добавить вагон");
         dateLabel = new JLabel("Дата поездки:");
-        // timeLabel = new JLabel("Время поездки:");
+        timeLabel = new JLabel("Время поездки:");
 
         titleLable.setBounds(150, 0, 100, 20);
         datePicker.setBounds(120, 30, 120, 25);
-        // timePicker.setBounds(120, 55, 120, 25);
+        timePicker.setBounds(120, 60, 250, 30);
         dateLabel.setBounds(20, 30, 100, 25);
-        // timeLabel.setBounds(20, 55, 100, 25);
+        timeLabel.setBounds(20, 60, 100, 25);
 
         add(titleLable);
         add(datePicker);
-        // add(timePicker);
+        add(timePicker);
         add(dateLabel);
-        // add(timeLabel);
+        add(timeLabel);
     }
 }
