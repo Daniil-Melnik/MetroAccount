14acb57088513c4ad31c722286fde397c16203f7
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-21 11:06:54 +0300

    added changes to enum of types carriages

--- src/main/java/com/accounts/Enums/CarriagesModels.java
+++ src/main/java/com/accounts/Enums/CarriagesModels.java
@@ -1,11 +1,17 @@
 package com.accounts.Enums;
 
 public enum CarriagesModels {
-    BALTIETS("81-725/726/727 «Балтиец»"),
-    YUBILEYNY("81-722/723/724 «Юбилейный»"),
-    EM("Ем"),
-    NEVA("81-556/557/558 НеВа"),
-    NOMERNOY1("81-717/714");
+    BALTIETS("«Балтиец» 81-725/726/727"),
+    EM("«Ем»"),
+    NEVA("«НеВа» 81-556/557/558 "),
+    NOMERNOY1("«Номерной» 81-717/714"),
+    NOMERNOY4("«Номерной» 81-717.5/714.5"),
+    NOMERNOY5("«Номерной» 81-717.5П/714.5П"),
+    NOMERNOY2("«Номерной» 81-540/541"),
+    NOMERNOY3("«Номерной» 81-540.2/541.2"),
+    NOMERNOY6("«Номерной» 81-540.5/541.5"),
+    NOMERNOY7("«Номерной» 81-540.7/541.7"),
+    YUBILEYNY("«Юбилейный» 81-722/723/724 ");
 
     private final String description;
 
