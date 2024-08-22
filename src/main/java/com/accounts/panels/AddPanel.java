f3eb7b15a119bad9454cfe99193b7bb0e8f9d54f
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-22 17:41:14 +0300

    added action listener

--- src/main/java/com/accounts/panels/AddPanel.java
+++ src/main/java/com/accounts/panels/AddPanel.java
@@ -3,6 +3,7 @@ package com.accounts.panels;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,6 +13,11 @@ import org.jdesktop.swingx.JXDatePicker;
 
 import com.accounts.Enums.CarriagesModels;
 import com.accounts.components.TimePicker;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import java.awt.Font;
 
 public class AddPanel extends JPanel {
 
@@ -24,10 +30,13 @@ public class AddPanel extends JPanel {
     private JLabel typeLabel;
     private JTextField numberCarriadge;
     private JLabel numberLabel;
+    private JButton submit;
 
     public AddPanel() {
         setLayout(null);
         setSize(400, 280);
+
+        Font font15 = new Font("Arial", Font.BOLD, 15);
 
         datePicker = new JXDatePicker();
         datePicker.setDate(Calendar.getInstance().getTime());
@@ -39,13 +48,17 @@ public class AddPanel extends JPanel {
 
         numberCarriadge = new JTextField();
 
+        submit = new JButton("Добавить");
+
         titleLable = new JLabel("Добавить вагон");
+        titleLable.setFont(font15);
+
         dateLabel = new JLabel("Дата поездки:");
         timeLabel = new JLabel("Время поездки:");
         typeLabel = new JLabel("Модель:");
         numberLabel = new JLabel("№ вагона:");
 
-        titleLable.setBounds(150, 0, 100, 20);
+        titleLable.setBounds(120, 5, 150, 20);
         datePicker.setBounds(120, 60, 120, 25);
         timePicker.setBounds(120, 90, 250, 30);
         dateLabel.setBounds(20, 60, 100, 25);
@@ -54,6 +67,7 @@ public class AddPanel extends JPanel {
         typeLabel.setBounds(20, 120, 50, 25);
         numberLabel.setBounds(20, 30, 60, 25);
         numberCarriadge.setBounds(120, 30, 100, 25);
+        submit.setBounds(0, 150, 390, 30);
 
         add(titleLable);
         add(datePicker);
@@ -64,5 +78,12 @@ public class AddPanel extends JPanel {
         add(typeLabel);
         add(numberCarriadge);
         add(numberLabel);
+        add(submit);
+
+        submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.out.println(carriadgeType.getSelectedIndex());
+            }
+        });
     }
 }
