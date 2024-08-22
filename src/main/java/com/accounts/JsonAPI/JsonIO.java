487d05da5c218c6bfbebc534f6b699df6507f829
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-22 17:24:08 +0300

    added test example for json

---
+++ src/main/java/com/accounts/JsonAPI/JsonIO.java
@@ -0,0 +1,23 @@
+package com.accounts.JsonAPI;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+public class JsonIO {
+    public static void main(String[] args) throws Exception {
+        JSONArray jsonObject = (JSONArray) readJsonArray(
+                "C:\\Users\\dmelnik\\Desktop\\вм\\MetroAccount\\src\\json\\vagons.json");
+        System.out.println(jsonObject);
+    }
+
+    public static Object readJsonArray(String filename) throws Exception {
+        FileReader reader = new FileReader(filename);
+        JSONParser jsonParser = new JSONParser();
+        Object obj = jsonParser.parse(reader);
+        JSONArray employeeList = (JSONArray) obj;
+
+        return employeeList;
+    }
+}
