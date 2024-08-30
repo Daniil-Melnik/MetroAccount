42a29c47e8feac8ed3f0fe32af8cebc4a1dd70f8
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-08-30 09:57:06 +0300

    added enum of factories

---
+++ src/main/java/com/accounts/Enums/Factories.java
@@ -0,0 +1,23 @@
+package com.accounts.Enums;
+
+public enum Factories {
+    LVZ("ЛВЗ им. И. Е. Егорова"),
+    VAGONMASH("Вагонмаш"),
+    OERVZ("ОЭВРЗ"),
+    MVM("Метровагонмаш");
+
+    private final String description;
+
+    Factories(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
