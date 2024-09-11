7a25db3e8be8a1b0537d8479605ea5e4a2a752d8
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-09-11 18:01:25 +0300

    created makeFileName(int vagonNumber)

--- src/main/java/com/accounts/FileAPI/FileManager.java
+++ src/main/java/com/accounts/FileAPI/FileManager.java
@@ -2,4 +2,19 @@ package com.accounts.FileAPI;
 
 public class FileManager {
 
+    public String makeFileName(int vagonNumber) {
+        String fileName = "";
+        String vagonNumberStr = Integer.toString(vagonNumber);
+        fileName = vagonNumberStr.substring(0, 2) + "xx.json";
+        return fileName;
+    }
+
+    // public void getMainFile(String fileName) {
+
+    // }
+
+    public static void main(String[] args) {
+        FileManager fm = new FileManager();
+        System.out.println(fm.makeFileName(6940));
+    }
 }
