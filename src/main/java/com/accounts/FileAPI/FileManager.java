5f4615d4f174be1f9fb6374ee59e7cd9c57c351f
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-09-11 18:48:28 +0300

    prepare for test FileAPI

--- src/main/java/com/accounts/FileAPI/FileManager.java
+++ src/main/java/com/accounts/FileAPI/FileManager.java
@@ -1,6 +1,9 @@
 package com.accounts.FileAPI;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 import com.accounts.Enums.MainEnum;
 
@@ -12,15 +15,29 @@ public class FileManager {
         return fileName;
     }
 
-    public FileReader getMainFile(int vagonNumber) {
-        String fileName = makeFileName(vagonNumber);
-        String fullFilename = MainEnum.MAIN_JSON_PATH + fileName;
-        System.out.println(fullFilename);
-        return null;
+    private void createFile(String fileName) throws IOException {
+        File f = new File(fileName);
+        f.createNewFile();
     }
 
-    public static void main(String[] args) {
-        FileManager fm = new FileManager();
-        fm.getMainFile(6940);
+    public FileReader getMainFile(int vagonNumber) throws FileNotFoundException {
+        FileReader file;
+        String fileName = makeFileName(vagonNumber);
+        String fullFilename = MainEnum.MAIN_JSON_PATH + fileName;
+
+        try {
+            createFile(fullFilename);
+        } catch (IOException e) {
+            // добавить логи
+        }
+
+        file = new FileReader(fullFilename);
+
+        return file;
     }
+
+    // public static void main(String[] args) {
+    // FileManager fm = new FileManager();
+
+    // }
 }
