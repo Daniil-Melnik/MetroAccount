cae247b3ec198e847993f029b26374989f1f68ad
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-09-27 18:13:04 +0300

    added transfer to ArrayList from JSONArray of String

--- src/main/java/com/accounts/JsonAPI/Typetransfer.java
+++ src/main/java/com/accounts/JsonAPI/Typetransfer.java
@@ -2,11 +2,12 @@ package com.accounts.JsonAPI;
 
 import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Typetransfer {
 
-    public Vagon toVagon(JSONObject obj){
+    public Vagon toVagon(JSONObject obj) {
         Vagon res;
         String number = obj.get("number").toString();
         String type = obj.get("type").toString();
@@ -19,11 +20,19 @@ public class Typetransfer {
         return res;
     }
 
-    public Vagon [] toVagonArray(ArrayList<Vagon> arrList){
-        Vagon [] res = new Vagon[arrList.size()];
-        for (int i = 0; i < arrList.size(); i++){
+    public Vagon[] toVagonArray(ArrayList<Vagon> arrList) {
+        Vagon[] res = new Vagon[arrList.size()];
+        for (int i = 0; i < arrList.size(); i++) {
             res[i] = arrList.get(i);
         }
         return res;
+    }
+
+    public ArrayList<String> toArrayList(JSONArray jsonArr) {
+        ArrayList<String> resArr = new ArrayList<>();
+        for (int i = 0; i < jsonArr.size(); i++) {
+            resArr.add(jsonArr.get(i).toString());
+        }
+        return resArr;
     }
 }
