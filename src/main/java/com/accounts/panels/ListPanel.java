7807b2900c9652696e3ebc9f1b8349cba8ee43a2
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-10-14 22:21:28 +0300

    added filter for list

--- src/main/java/com/accounts/panels/ListPanel.java
+++ src/main/java/com/accounts/panels/ListPanel.java
@@ -13,37 +13,40 @@ import com.accounts.JsonAPI.JsonIO;
 import com.accounts.JsonAPI.JsonParser;
 
 import java.awt.Font;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.IOException;
 
 public class ListPanel extends JPanel{
 
     private JLabel titleLable;
     private JComboBox<String> fileNameCombo;
+    private String [][] data;
+    private JTable table;
+    private JScrollPane scrollPane;
+    private String[] columnNames = { "Номер", "Тип", "Завод", "Год вып.", "Линия", "Дата", "Время" };
+    DefaultTableModel model;
 
     public ListPanel(){
         setLayout(null);
-        setSize(730, 280);
-
-        JsonParser jsonParser = new JsonParser();
-
-        String[] columnNames = { "Номер", "Тип", "Завод", "Год вып.", "Линия", "Дата", "Время" };
-
-        String[][] data;
+        setSize(730, 280);        
 
         String [] comboData = makeComboFileNames();
+        titleLable = new JLabel("Список Вагонов");
+        fileNameCombo = new JComboBox<>(comboData);
+
+        fileNameCombo.addItemListener(new ItemListener() {
+            @Override public void itemStateChanged(ItemEvent e) {
+                fileNameComboListener();
+            }
+        });
 
         try {
-            data = jsonParser.getAllVagonsString();
-            DefaultTableModel model = new DefaultTableModel(data, columnNames);
-            JTable table = new JTable(model);
-            table.getColumnModel().getColumn(0).setPreferredWidth(20);
-            table.getColumnModel().getColumn(1).setPreferredWidth(100);
-            table.getColumnModel().getColumn(2).setPreferredWidth(100);
-            table.getColumnModel().getColumn(3).setPreferredWidth(20);
-            table.getColumnModel().getColumn(4).setPreferredWidth(150);
-            table.getColumnModel().getColumn(5).setPreferredWidth(40);
-            table.getColumnModel().getColumn(6).setPreferredWidth(15);
-            JScrollPane scrollPane = new JScrollPane(table);
+            data = getVagonsForTable();
+            model = new DefaultTableModel(data, columnNames);
+            table = new JTable(model);
+            setColumnWidths();
+            scrollPane = new JScrollPane(table);
             scrollPane.setBounds(0, 25, 718, 230);
             add(scrollPane);
 
@@ -53,9 +56,6 @@ public class ListPanel extends JPanel{
         }
 
         Font font15 = new Font("Arial", Font.BOLD, 15);
-
-        titleLable = new JLabel("Список Вагонов");
-        fileNameCombo = new JComboBox<>(comboData);
 
         titleLable.setFont(font15);
         
@@ -78,5 +78,41 @@ public class ListPanel extends JPanel{
         System.arraycopy(fileNumberNames, 0, comboData, addAll.length, fileNumberNames.length);
 
         return comboData;
+    }
+
+    private String [][] getVagonsForTable() throws IOException, ParseException{
+        JsonParser jsonParser = new JsonParser();
+        String [][] dataM;
+        String comboValue = fileNameCombo.getSelectedItem().toString();
+        System.out.println(comboValue);
+        if (comboValue.equals("...")){
+            dataM = jsonParser.getAllVagonsString();
+        }
+        else {
+            dataM = jsonParser.getVagonsOfFileString(comboValue);
+        }
+        return dataM;
+    }
+
+    private void setColumnWidths(){
+        table.getColumnModel().getColumn(0).setPreferredWidth(20);
+        table.getColumnModel().getColumn(1).setPreferredWidth(100);
+        table.getColumnModel().getColumn(2).setPreferredWidth(100);
+        table.getColumnModel().getColumn(3).setPreferredWidth(20);
+        table.getColumnModel().getColumn(4).setPreferredWidth(150);
+        table.getColumnModel().getColumn(5).setPreferredWidth(40);
+        table.getColumnModel().getColumn(6).setPreferredWidth(15);
+    }
+
+    private void fileNameComboListener(){
+        try {
+            data = getVagonsForTable();
+            model.setDataVector(data, columnNames);
+            setColumnWidths();
+            scrollPane.repaint();
+            scrollPane.revalidate();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
