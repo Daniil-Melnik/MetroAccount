898a4a86325097bf4c76934ba42c7ae6a94e16cc
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-10-14 21:35:48 +0300

    added fileNameCombo for vagon list

--- src/main/java/com/accounts/panels/ListPanel.java
+++ src/main/java/com/accounts/panels/ListPanel.java
@@ -3,11 +3,13 @@ package com.accounts.panels;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
 
 import org.json.simple.parser.ParseException;
 
+import com.accounts.JsonAPI.JsonIO;
 import com.accounts.JsonAPI.JsonParser;
 
 import java.awt.Font;
@@ -16,6 +18,7 @@ import java.io.IOException;
 public class ListPanel extends JPanel{
 
     private JLabel titleLable;
+    private JComboBox<String> fileNameCombo;
 
     public ListPanel(){
         setLayout(null);
@@ -26,6 +29,8 @@ public class ListPanel extends JPanel{
         String[] columnNames = { "Номер", "Тип", "Завод", "Год вып.", "Линия", "Дата", "Время" };
 
         String[][] data;
+
+        String [] comboData = makeComboFileNames();
 
         try {
             data = jsonParser.getAllVagonsString();
@@ -50,12 +55,28 @@ public class ListPanel extends JPanel{
         Font font15 = new Font("Arial", Font.BOLD, 15);
 
         titleLable = new JLabel("Список Вагонов");
+        fileNameCombo = new JComboBox<>(comboData);
 
         titleLable.setFont(font15);
         
         titleLable.setBounds(170, 5, 150, 20);
+        fileNameCombo.setBounds(330, 5, 60, 20);
 
         add(titleLable);
+        add(fileNameCombo);
     }
-    
+
+    private String [] makeComboFileNames(){
+        JsonIO jsonIO = new JsonIO();
+
+        String [] fileNumberNames = jsonIO.getHeadFileNumbersString();
+        String [] addAll = {"..."};
+
+        String [] comboData = new String[1 + fileNumberNames.length];
+
+        System.arraycopy(addAll, 0, comboData, 0, addAll.length);
+        System.arraycopy(fileNumberNames, 0, comboData, addAll.length, fileNumberNames.length);
+
+        return comboData;
+    }
 }
